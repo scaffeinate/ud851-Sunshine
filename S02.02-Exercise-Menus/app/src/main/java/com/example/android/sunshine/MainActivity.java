6c8540546cce5774d75609b70eadaacf2e61dbd2
Sudharsanan <sudhar.ti@gmail.com>
2017-10-05 19:54:58 -0400

    Fix clearing the output

--- S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -120,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_refresh:
+                mWeatherTextView.setText("");
                 loadWeatherData();
                 return true;
         }
