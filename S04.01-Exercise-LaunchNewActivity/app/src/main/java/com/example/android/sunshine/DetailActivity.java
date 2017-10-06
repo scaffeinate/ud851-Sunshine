afc4d612765b74d83fa6e4a543900d4bf48ce8e0
Sudharsanan <sudhar.ti@gmail.com>
2017-10-06 01:52:50 -0400

    Completed S04.01 and 02

---
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sudhar on 10/6/17.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,31 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    public static final String WEATHER_DETAIL = "weather_detail";
+
+    private TextView mWeatherDetailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        mWeatherDetailTextView = (TextView) findViewById(R.id.tv_weather_detail);
+
+        // COMPLETED (2) Display the weather forecast that was passed from MainActivity
+        Intent intent = getIntent();
+        if (intent.hasExtra(WEATHER_DETAIL)) {
+            String weatherDetail = intent.getStringExtra(WEATHER_DETAIL);
+            if (weatherDetail != null && !weatherDetail.isEmpty()) {
+                mWeatherDetailTextView.setText(weatherDetail);
+            }
+        }
     }
 }
