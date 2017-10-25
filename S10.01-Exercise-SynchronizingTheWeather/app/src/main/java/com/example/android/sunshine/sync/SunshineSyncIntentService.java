29e11467ed2e11e467d17dbd69de4285b40de664
Sudharsanan <sudhar.ti@gmail.com>
2017-10-25 00:24:49 -0400

    Finish up S10.01

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -1,3 +1,25 @@
-// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
-//  TODO (6) Create a constructor that calls super and passes the name of this class
-//  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+package com.example.android.sunshine.sync;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+// COMPLETED (5) Create a new class called SunshineSyncIntentService that extends IntentService
+//  COMPLETED (6) Create a constructor that calls super and passes the name of this class
+//  COMPLETED (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+public class SunshineSyncIntentService extends IntentService {
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public SunshineSyncIntentService() {
+        super(SunshineSyncIntentService.class.getName());
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        SunshineSyncTask.syncWeather(this);
+    }
+}
